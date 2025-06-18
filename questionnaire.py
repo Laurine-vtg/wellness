4cb1586735dd55152b3cd98de9fab400aa8acffb
Laurine-vtg <152720415+Laurine-vtg@users.noreply.github.com>
2025-06-18 14:34:35 +0200

    questionnaire.py

--- questionnaire.py
+++ questionnaire.py
@@ -713,14 +713,6 @@ elif page == "Compte rendu individuel (joueur)":
      fin_semaine = semaine_selectionnee + timedelta(days=6)
      df_semaine = df_user[(df_user["Date"] >= debut_semaine) & (df_user["Date"] <= fin_semaine)]
 
-     try:
-        locale.setlocale(locale.LC_TIME, 'fr_FR.UTF-8')  
-     except:
-        try:
-            locale.setlocale(locale.LC_TIME, 'French')  
-        except:
-            st.warning("Impossible d'afficher les jours en français (paramètre régional manquant)")
-
     # -------------------------------------------------- Graphique intensité -----------------------------------------------------
      if prefs["show_weekly_intensity"] and not df_semaine.empty:
         jours_semaine = [debut_semaine + timedelta(days=i) for i in range(7)]
